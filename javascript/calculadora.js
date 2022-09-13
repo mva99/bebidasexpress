0733f7b5b202620dbe854fd253605abf078abbdd
Matheus Victor Almeida <matheusvictoralmeida1999@gmail.com>
2022-09-13 01:28:37 -0300

    finalização-calculadora

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -12,7 +12,38 @@ function calcularBebidas() {
     spansQtd = document.getElementsByClassName("span-qtd-bebida");
 
     Array.prototype.forEach.call(spansQtd, function(item) {
-        item.innerHTML = " x " + document.getElementById('input_qtd_adultos').value;
+        var idQtdBebida = item.id;
+        idQtdBebida = idQtdBebida.replace("span-qtd-bebida-", "qtd-");
+
+        var qtdBebida = document.getElementById(idQtdBebida).value;
+        var qtdAdultos = document.getElementById('input_qtd_adultos').value;
+
+        //caso seja energético
+        if (item.id.replace("span-qtd-bebida-", "") == "energetico") {
+            var qtdEnergetico = Math.ceil(eval(qtdAdultos) / eval(qtdBebida));
+            var qtdEnergeticoVodka = 0;
+            var qtdEnergeticoGim = 0;
+
+            //5 energéticos para cada vodka
+            if (document.getElementById('row-vodka').style.display != 'none') {
+                var qtdVodka = document.getElementById('qtd-vodka').value;
+                qtdEnergeticoVodka = Math.ceil(eval(qtdAdultos) / eval(qtdVodka))
+            }
+
+            //5 energéticos para cada gim
+            if (document.getElementById('row-gim').style.display != 'none') {
+                var qtdGim = document.getElementById('qtd-gim').value;
+                qtdEnergeticoGim = Math.ceil(eval(qtdAdultos) / eval(qtdGim))
+            }
+
+            qtdEnergetico += qtdEnergeticoGim * 5;
+            qtdEnergetico += qtdEnergeticoVodka * 5;
+
+            item.innerHTML = " x " + qtdEnergetico;
+        } else {
+            var qtdBebidaTotal = Math.ceil(eval(qtdAdultos) / eval(qtdBebida));
+            item.innerHTML = " x " + qtdBebidaTotal;
+        }
     });
 }
 
@@ -23,6 +54,7 @@ document.getElementById('Bebida1').onclick = function() {
     } else {
         document.getElementById("bebidas-alcoolicas").style.display = "none";
     }
+    calcularBebidas();
 }
 
 document.getElementById('Bebida2').onclick = function() {
@@ -32,6 +64,7 @@ document.getElementById('Bebida2').onclick = function() {
     } else {
         document.getElementById("bebidas-nao-alcoolicas").style.display = "none";
     }
+    calcularBebidas();
 }
 
 document.getElementById('Bebida3').onclick = function() {
@@ -41,6 +74,7 @@ document.getElementById('Bebida3').onclick = function() {
     } else {
         document.getElementById("bebidas-destiladas").style.display = "none";
     }
+    calcularBebidas();
 }
 
 function SelecionarBebida(tipo_bebida) {
@@ -51,4 +85,5 @@ function SelecionarBebida(tipo_bebida) {
     } else {
         document.getElementById("row-" + tipo_bebida).style.display = "none";
     }
+    calcularBebidas();
 }
